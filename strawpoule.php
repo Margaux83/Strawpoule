064fa1c221f0ff4347f06c25ad899ede6cd1d617
ronantr <noob93500@gmail.com>
2021-07-02 12:56:57 +0200

    ajoute et insitalisation de admin/poule_admin.php

--- strawpoule.php
+++ strawpoule.php
@@ -1,7 +1,16 @@
 <?php
 /**
  * Plugin Name: Strawpoule
- * Plugin URI: https://github.com/Margaux83/Wordpress-Project
+ * Plugin URI: https://github.com/Margaux83/Strawpoule
  * Description:
- *
+ * Version: 0.1
+ * Liencse: GPL2
  */
+
+define ("POULE_MIAN_FILE",__FILE__);
+
+define('PLUGIN_DIR', dirnam(__FILE__).'/');
+
+require_once ('poule_init.php');
+
+require_once ('admin/poule_admin.php');
