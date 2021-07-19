a4df187eb9281ebb2e7df56ed02220662a54dd85
ronantr <noob93500@gmail.com>
2021-07-19 18:52:58 +0200

    modficiation de la page admin_poll.php

--- views/admin_poll.php
+++ views/admin_poll.php
@@ -1,3 +1,17 @@
 <?php
 
 echo 'hello';
+?>
+<!DOCTYPE>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<?php wp_head(); ?>
+</head>
+<body>
+<h1> Sondage</h1>
+
+
+</body>
+</html>
