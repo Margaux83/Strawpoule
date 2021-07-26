d55dafbf635b13a0d4ecdcab9bc15569b44e258c
Margaux83 <margauxhebert83@gmail.com>
2021-07-27 00:06:49 +0200

    possibilité d'ajouter un input sur la view, quand on veut supprimer un input, ça supprime tous les input

--- includes/views/admin_poll_add.php
+++ includes/views/admin_poll_add.php
@@ -1,9 +1,8 @@
 <div class="wrap">
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
     <form method="post">
-        <div>
+        <div class="wrapper">
             <table class="form-table" role="presentation" id="createuser">
-                <tr class="form-field form-required">
                 <tr class="form-field form-required">
                     <th>
                         <label>Titre du sondage</label>
@@ -27,7 +26,7 @@
                         <input type="checkbox" id="horns" name="horns">
                     </td>
                 </tr>
-                <tr class="form-field form-required">
+                <tr class="form-field form-required ">
                     <th>
                         <label>#1</label>
                     </th>
@@ -43,35 +42,92 @@
                         <input type="text" name="strawpoule_reponse_2" value="test" required />
                     </td>
                 </tr>
-                <tr class="form-field">
+                <tr class="form-field form-required">
                     <th>
                         <label>#3</label>
                     </th>
                     <td>
                         <input type="text" name="strawpoule_reponse_3" value="test"/>
-                        <a href="#">Supprimer</a>
-
                     </td>
+                </tr>
+                <tr class="form-field form-required reponse">
                 </tr>
                 <tr>
                     <td>
-                        <a href="#" id="add-response">Ajouter une réponse</a>
+                        <button id="add-response">Ajouter une réponse</button>
                     </td>
                 </tr>
             </table>
         </div>
         <?php
-        //wp_nonce_field();
         submit_button();
         ?>
     </form>
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
-    var response_component = {
+    /*var response_component = {
 
     }
     jQuery('#add-response').on('click', function() {
 
         alert('qzd');
-    })
+    })*/
+
+    /*$(document).ready(function() {
+        var max_fields = 10;
+        var wrapper = $(".form-field.form-required.reponse");
+        var add_button = $("#add-response");
+
+        var x = 1;
+        $(add_button).click(function(e) {
+            e.preventDefault();
+            if (x < max_fields) {
+                x++;
+                $(wrapper).append(
+                    '   <th class="form-field-add">' +
+                    '   <label>#'+
+                    '</label>' +
+                    ' </th>' +
+                    ' <td>' +
+                    '<div><input type="text" name="strawpoule_reponse_3"/><button class="delete">Supprimer</button></div>' +
+
+                    '  </td><br>' );
+            } else {
+                alert('You Reached the limits')
+            }
+        });
+
+        $(wrapper).on("click", ".delete", function(e) {
+            e.preventDefault();
+            $('.form-field-add').remove();
+            x--;
+        })
+    });*/
+    var x = 1;
+        $('#add-response').click(function(e) {
+
+            e.preventDefault();
+            const wrapper = $(".form-field.form-required.reponse");
+            $(wrapper).append(
+                    '   <th class="form-field-add">' +
+                    '   <label>#'+
+                    '</label>' +
+                    ' </th>' +
+                    ' <td>' +
+                    '<div><input type="text" name="strawpoule_reponse_3"/><button class="delete">Supprimer</button></div>' +
+
+                    '  </td><br>' );
+        });
+
+
+    /*$('.form-field.form-required.reponse').on("click", ".delete", function(e) {
+        e.preventDefault();
+        $('.form-field-add').remove();
+        x--;
+    })*/
+   $('.delete').click( function(e) {
+            e.preventDefault();
+         $('.form-field-add').remove();
+        });
 </script>
